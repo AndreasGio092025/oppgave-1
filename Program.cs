3d054c8c33760f9d841ed8480a195ebbce04af51
Andreas092025 <andreasgio74@gmail.com>
2025-10-08 14:46:44 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+    
+    int year = 2025;
+    int month = 12;
+
+for (int day = 1; day <= 31; day++)
+ {
+
+    DateTime date = new DateTime(year, month, day);
+
+    Console.WriteLine(date.ToString("d.M.yyyy"));
+
+    if (date.DayOfWeek == DayOfWeek.Tuesday)
+     {
+        Console.WriteLine("good Tuesday");
+     }
+     else
+     {
+     Console.WriteLine("it's not Tuesday");
+
+     }
+    Console.WriteLine();
+}
+
+    
+
+
+
+    
+
+
+
+
+    
